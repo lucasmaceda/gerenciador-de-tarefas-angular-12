7ce15161aed0c6ce8dbdd50eede8eedea7aef29d
Lucas Maceda Miranda <lucasmacedamiranda@gmail.com>
2021-11-04 14:34:55 -0300

    project files

---
+++ src/app/tarefas/tarefas.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+
+import { TarefaService, TarefaConcluidaDirective } from './shared';
+import { ListarTarefaComponent } from './listar-tarefa';
+import { CadastrarTarefaComponent } from './cadastrar-tarefa/cadastrar-tarefa.component';
+import { EditarTarefaComponent } from './editar-tarefa/editar-tarefa.component';
+
+
+@NgModule({
+  imports: [
+    CommonModule,
+    RouterModule,
+    FormsModule
+  ],
+  declarations: [
+    ListarTarefaComponent,
+    CadastrarTarefaComponent,
+    EditarTarefaComponent,
+    TarefaConcluidaDirective,
+  ],
+  providers: [
+    TarefaService
+  ]
+})
+export class TarefasModule { }
